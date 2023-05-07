a4dff5c4c6562783c236620086d7b8336ee0f8c0
Quentin Burgniard <quentinburgniard@gmail.com>
2023-05-07 16:54:06 +0200

    feat(design-system)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -28,28 +28,58 @@
 </script>
 
 <template>
-  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-4 offset-lg-4">
+      <div class="ds-title">
+        <h1 class="_ds-title title">Sign In</h1>
+      </div>
+    </div>
+  </div>
+  <form class="ds-form" @submit.prevent="login">
     <div class="row">
-      <div class="container">
-        <div class="col-lg-4 offset-lg-4">
-          <h2>Sign In</h2>
-          <form class="needs-validation" :class="{ 'was-validated': validated }" novalidate @submit.prevent="login">
-            <div class="mt-3">
-              <input class="form-control" id="email" placeholder="Email" required type="email" v-model.lazy="email">
-              <div class="invalid-feedback">
-                Email is required.
-              </div>
+      <div class="col-lg-4 offset-lg-4">
+        <div class="_ds-form text">
+          <label for="email">Email</label>
+          <input autocapitalize="none" id="email" name="email" placeholder="contact@quentinburgniard.com" required type="email" v-model.lazy="email">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 offset-lg-4">
+        <div class="_ds-form text">
+          <label for="password">Password</label>
+          <input id="password" name="password" required type="password" v-model.lazy="password">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 offset-lg-4">
+        <button class="ds-link" type="submit">
+          <div class="_ds-link body">
+            <div class="_ds-link title">Sign In</div>
+          </div>
+          <div class="_ds-link action-icon">
+            <i class="bi bi-box-arrow-in-right"></i>
+          </div>
+        </button>
+      </div>
+    </div>
+  </form>
+  <div class="ds-menu z-index-medium" id="ds-menu">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <a alt="Français" class="ds-link" href="/">
+            <div class="_ds-link caption-icon">
+              <i class="bi bi-globe"></i>
             </div>
-            <div class="mt-3">
-              <input class="form-control" id="password" placeholder="Password" required type="password" v-model.lazy="password">
-              <div class="invalid-feedback">
-                Password is required.
-              </div>
+            <div class="_ds-link body">
+              <div class="_ds-link title">Français</div>
             </div>
-            <div class="mt-3">
-              <button class="btn btn-primary">Sign In</button>
+            <div class="_ds-link action-icon">
+              <i class="bi bi-chevron-right"></i>
             </div>
-          </form>
+          </a>
         </div>
       </div>
     </div>
--- src/components/Reset.vue
+++ src/components/Reset.vue
@@ -26,34 +26,34 @@
 </script>
 
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="container">
-        <div class="col-lg-4 offset-lg-4">
-          <form class="needs-validation" :class="{ 'was-validated': validated }" novalidate @submit.prevent="reset">
-            <div class="mt-3">
-              <input class="form-control" id="email" placeholder="Email" required type="email" v-model.lazy="email">
-              <div class="invalid-feedback">
-                Email is required.
-              </div>
+  <div class="row">
+    <div class="container">
+      <div class="col-lg-4 offset-lg-4">
+        <form class="needs-validation" :class="{ 'was-validated': validated }" novalidate @submit.prevent="reset">
+          <div class="mt-3">
+            <input class="form-control" id="email" placeholder="Email" required type="email" v-model.lazy="email">
+            <div class="invalid-feedback">
+              Email is required.
             </div>
-            <div class="mt-3">
-              <input class="form-control" id="password" placeholder="Password" required type="password" v-model.lazy="password">
-              <div class="invalid-feedback">
-                Password is required.
-              </div>
+          </div>
+          <div class="mt-3">
+            <input class="form-control" id="password" placeholder="Password" required type="password"
+              v-model.lazy="password">
+            <div class="invalid-feedback">
+              Password is required.
             </div>
-            <div class="mt-3">
-              <input class="form-control" id="passwordConfirmation" placeholder="Password Confirmation" required type="password" v-model.lazy="passwordConfirmation">
-              <div class="invalid-feedback">
-                Password confirmation is required.
-              </div>
+          </div>
+          <div class="mt-3">
+            <input class="form-control" id="passwordConfirmation" placeholder="Password Confirmation" required
+              type="password" v-model.lazy="passwordConfirmation">
+            <div class="invalid-feedback">
+              Password confirmation is required.
             </div>
-            <div class="mt-3">
-              <button class="btn btn-primary">Reset Password</button>
-            </div>
-          </form>
-        </div>
+          </div>
+          <div class="mt-3">
+            <button class="btn btn-primary">Reset Password</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
